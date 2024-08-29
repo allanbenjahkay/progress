479bc6d1052053bffaf795e1b5cd171a200185c9
dev global <allanbenjahkay@gmail.com>
2024-08-29 23:32:21 +0300

    modified login

--- index.php
+++ index.php
@@ -1,39 +1,46 @@
-<?php include('server.php') ?>
+<?php 
+  session_start(); 
 
+  if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
-
 <html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Form</title>
-  <link rel="stylesheet" href="style.css">
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div class="wrapper">
-    <form action="index.php">
-    <?php include('errors.php'); ?>
-      <h2>Login</h2>
-        <div class="input-field">
-        <input type="text" required>
-        <label>Enter your email</label>
+
+<div class="header">
+        <h2>Home Page</h2>
+</div>
+<div class="content">
+        <!-- notification message -->
+        <?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+        <h3>
+          <?php 
+                echo $_SESSION['success']; 
+                unset($_SESSION['success']);
+          ?>
+        </h3>
       </div>
-      <div class="input-field">
-        <input type="password" required>
-        <label>Enter your password</label>
-      </div>
-      <div class="forget">
-        <label for="remember">
-          <input type="checkbox" id="remember">
-          <p>Remember me</p>
-        </label>
-        <a href="#">Forgot password?</a>
-      </div>
-      <button type="submit">Log In</button>
-      <div class="register">
-        <p>Don't have an account? <a href="Register.html">Register</a></p>
-      </div>
-    </form>
-  </div>
+        <?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['name'])) : ?>
+        <p>Welcome <strong><?php echo $_SESSION['name']; ?></strong></p>
+        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+                
 </body>
 </html>
